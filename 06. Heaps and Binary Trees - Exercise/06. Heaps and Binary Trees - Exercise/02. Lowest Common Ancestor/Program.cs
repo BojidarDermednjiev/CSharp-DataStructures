22d9505c7cadc441cda3c1e571392e868bcd5a8b
Bojidar Dermendjiew <bojidardermendjiew@abv.bg>
2023-09-06 15:43:54 +0300

    adding the rest of exercise

---
+++ 06. Heaps and Binary Trees - Exercise/06. Heaps and Binary Trees - Exercise/02. Lowest Common Ancestor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02._Lowest_Common_Ancestor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 06. Heaps and Binary Trees - Exercise/06. Heaps and Binary Trees - Exercise/03. Min Heap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Min_Heap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
